d611034047cfffd586f716d936b9d0f21b88f762
ANSH SINGH <139059815+SinghAnsh07@users.noreply.github.com>
2026-02-03 21:45:16 +0530

    Time: 0 ms (100.00%) | Memory: 28 MB (46.28%) - LeetSync

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.cpp
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.cpp
@@ -18,7 +18,7 @@ public:
     }
 
     int maxpathdown(TreeNode* node, int &maxi){
-        if(node == NULL) return -2;
+        if(node == NULL) return 0;
         int left = max(0, maxpathdown(node -> left, maxi));
         int right = max(0, maxpathdown(node -> right, maxi));
         maxi = max(maxi, left + right + node ->  val);
