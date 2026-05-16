a4016679473b9c8d89a09f0a1ece8728a6c3ef7e
ANSH SINGH <anshsingh50636@gmail.com>
2026-05-16 10:17:29 +0530

    Time: 0 ms (100.00%) | Memory: 12.8 MB (59.21%) - LeetSync

---
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int, int> nge;
+        stack<int> st;
+
+        for(int x : nums2){
+            while(!st.empty() && st.top() < x){
+                nge[st.top()] = x;
+                st.pop();
+            }
+            st.push(x);
+        }
+
+        while(!st.empty()){
+            nge[st.top()] = -1;
+            st.pop();
+        }
+
+        vector<int> ans;
+        for(int x : nums1){
+            ans.push_back(nge[x]);
+        }
+        return ans;
+    }
+};
