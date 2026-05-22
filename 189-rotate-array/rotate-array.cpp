05304ef6adb280e9e7cde6a9d310128806be7822
ANSH SINGH <anshsingh50636@gmail.com>
2026-05-22 20:28:46 +0530

    Time: 0 ms (100.00%) | Memory: 30.6 MB (11.83%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -5,12 +5,13 @@ public:
         k = k % n;
         vector<int> rotated(n);
 
-        for (int i = 0; i < n; i++) {
-            rotated[(i + k) % n] = nums[i];
+        for(int i=0; i< n ; i++){
+            rotated[ (i + k) % n ]= nums[i];
         }
 
-        for (int i = 0; i < n; i++) {
-            nums[i] = rotated[i];
-        }        
+        for(int i=0; i< n; i++){
+            nums[i]= rotated[i];
+        }
+
     }
 };
